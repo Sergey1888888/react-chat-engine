fcd124a13298e29b3412c406329fd7d69e94551d
Sergei <46000753+Sergey1888888@users.noreply.github.com>
2021-04-25 15:27:50 +0300

    Update dateToString.js

--- src/components/ChatEngine/Utilities/dateToString.js
+++ src/components/ChatEngine/Utilities/dateToString.js
@@ -1,10 +1,10 @@
 export function timeSinceDate(date) {
+    const months = ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
     if (!date) return ''
-    console.log(`DATE: ${date}`);
+    console.log(`DATE: ${date.toString()}`);
     const day = date.substr(8,2)
-    const month = date.substr(5,2)
+    const month = months[parseInt(date_time.substr(5, 2)) - 1]
     const year = date.substr(0,4)
-
     const hour = date.substr(11,2)
     const minute = date.substr(14,2)
     const second = date.substr(17,2)
