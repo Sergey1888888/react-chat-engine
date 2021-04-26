7e9159ec8b310304784c0b30af6a7cec21159107
Sergei <46000753+Sergey1888888@users.noreply.github.com>
2021-04-26 03:22:39 +0300

    Update dateToString.js

--- src/components/ChatEngine/Utilities/dateToString.js
+++ src/components/ChatEngine/Utilities/dateToString.js
@@ -5,7 +5,7 @@ export function timeSinceDate(date) {
     const day = date.substr(8,2)
     const month = months[parseInt(date.substr(5, 2)) - 1]
     const year = date.substr(0,4)
-    const hour = date.substr(11,2)
+    const hour = +date.substr(11,2) + 3 >= 24 ? +date.substr(11,2) - 21 : +date.substr(11,2) + 3;
     const minute = date.substr(14,2)
     const second = date.substr(17,2)
 
