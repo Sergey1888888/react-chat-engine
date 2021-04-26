2d853225f13ccf32d79aa2d940d45675d1294737
Sergei <46000753+Sergey1888888@users.noreply.github.com>
2021-04-27 00:04:00 +0300

    Update Typers.js

--- src/components/ChatEngine/ChatFeed/Typers.js
+++ src/components/ChatEngine/ChatFeed/Typers.js
@@ -5,9 +5,10 @@ import { ChatEngineContext } from '../../Context'
 import { stringToColor } from '../Utilities/colorMapping'
 
 const Typers = props => {
-    const { conn, activeChat, typingCounter } = useContext(ChatEngineContext)
+    const { conn, activeChat, typingCounter, chats } = useContext(ChatEngineContext)
     const typers = typingCounter && typingCounter[activeChat] ? typingCounter[activeChat] : []
-
+    const chat = chats && chats[currentChat]
+    
     if (!conn || conn === null) return <div />
 
     if (props.renderIsTyping) { return props.renderIsTyping(typers) }
@@ -16,14 +17,13 @@ const Typers = props => {
         <div>
             {
                 Object.keys(typers).map((username, index) => {
-                    console.log(username)
                     if (conn.userName !== username && props.currentTime < typers[username] + 2000) {
                         return (
                             <div 
                                 key={`typer_${index}`} 
                                 style={{ color: stringToColor(username), padding: '2px', paddingLeft: '12px' }}
                             >
-                                {`${username} печатает...`}
+                                {`${chat.people.filter(person => console.log(person))} печатает...`}
                             </div>
                         )
 
